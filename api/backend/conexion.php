e715bca1bc1968ddecc7d0c200b165746c9d62e3
Mario Zapata <mrzapata74@gmail.com>
2020-12-05 23:16:21 -0600

    Conexión a Base de Datos
    Creada conexión a la Base de Datos, para fin de consumir la API.

---
+++ api/backend/conexion.php
@@ -0,0 +1,18 @@
+<?php
+
+#CREANDO LA CONEXION
+
+$link = 'mysql:host=localhost;dbname=api';
+$usuario = 'root';
+$pass = '';
+
+try {
+  $pdo = new PDO($link,$usuario,$pass);
+  
+  } catch (PDOException $e) {
+  print "¡Error!:". $e->getMessage() . "<br/>";
+  die();
+}
+
+
+
