4ceec307d01ee430fd0495386a806bbf4c124664
Mario Zapata <mrzapata74@gmail.com>
2020-12-05 23:17:06 -0600

    Consulta a la Base de Datos
    Creación de Consulta a la Base de Datos

--- api/backend/datos.php
+++ api/backend/datos.php
@@ -1,6 +1,18 @@
 <?php
 
-$datos = ['dolar'=>500,'euro'=>700];
+header("Access-Control-Allow-Origin: *"); #Permite dar acceso a cualquier a consumir la API
+                                          #Si deseas consumir solo tú, sustituye el asterisco por tu dominio  
 
-echo json_encode($datos);
+if($_GET['moneda']=='euro' || $_GET['moneda']=='dolar'){
+    include_once 'conexion.php';
+
+    $sql = 'SELECT * FROM '.$_GET['moneda'];
+    $sentencia = $pdo ->prepare($sql);
+    $sentencia -> execute();
+    $databd = $sentencia->fetchAll();
+
+    
+}
+
+echo json_encode($databd);
 
